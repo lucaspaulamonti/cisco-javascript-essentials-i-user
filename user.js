1dcfafb92ea65d9cc9bd4c6e5c6d36943a4cdafb
lucaspaulamonti <lucasdepaulamonti@gmail.com>
2022-10-18 00:18:01 -0300

    Create user.js

---
+++ user.js
@@ -0,0 +1,17 @@
+// Criando dois objetos "user" e imprimindo o atributo "name" de cada um deles.
+let user1 = {
+    name: "Calvin",
+    surname: "Hart",
+    age: 66,
+    email: "CalvinMHart@teleworm.us"
+};
+
+let user2 = {
+    name: "Mateus",
+    surname: "Pinto",
+    age: 21,
+    email: "MateusPinto@dayrep.com"
+};
+
+console.log(user1.name);
+console.log(user2.name);
